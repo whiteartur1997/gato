da76b9e2f032a52a226c2093e1f2724ff156176f
Gotlibbb <gleb.evgenevichh@gmail.com>
2021-03-06 14:51:52 +0300

    active button <NavLink className={styles.navLink} to={PATH.PROFILE}>
      <li className={styles.navItem}>
                    Profile
      </li>
    </NavLink>

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,32 +1,51 @@
-import { NavLink } from "react-router-dom"
-import { PATH } from "../../components/Routes/Routes"
+import {NavLink} from "react-router-dom"
+import {PATH} from "../../components/Routes/Routes"
 import styles from './Header.module.css'
 
 export const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className={styles.nav}>
-        <ul className={styles.navList}>
-          <li className={styles.navItem}>
-            <NavLink className={styles.navLink} to={PATH.PROFILE}>Profile</NavLink>
-          </li>
-          <li className={styles.navItem}>
-            <NavLink className={styles.navLink} to={PATH.REGISTRATION}>Registration</NavLink>
-          </li>
-          <li className={styles.navItem}>
-            <NavLink className={styles.navLink} to={PATH.LOGIN}>Login</NavLink>
-          </li>
-          <li className={styles.navItem}>
-            <NavLink className={styles.navLink} to={PATH.NEWPASSWORD}>New Password</NavLink>
-          </li>
-          <li className={styles.navItem}>
-            <NavLink className={styles.navLink} to={PATH.FORGOTPASSWORD}>Forgot Password</NavLink>
-          </li>
-          <li className={styles.navItem}>
-            <NavLink className={styles.navLink} to={PATH.TESTCOMPONENTS}>Test</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+      <div className={styles.header}>
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+
+            <NavLink className={styles.navLink} to={PATH.PROFILE}>
+              <li className={styles.navItem}>
+                Profile
+              </li>
+            </NavLink>
+
+            <NavLink className={styles.navLink} to={PATH.REGISTRATION}>
+              <li className={styles.navItem}>
+                Registration
+              </li>
+            </NavLink>
+
+            <NavLink className={styles.navLink} to={PATH.LOGIN}>
+              <li className={styles.navItem}>
+                Login
+              </li>
+            </NavLink>
+
+            <NavLink className={styles.navLink} to={PATH.NEWPASSWORD}>
+              <li className={styles.navItem}>
+                New Password
+              </li>
+            </NavLink>
+
+            <NavLink className={styles.navLink} to={PATH.FORGOTPASSWORD}>
+              <li className={styles.navItem}>
+                Forgot Password
+              </li>
+            </NavLink>
+
+            <NavLink className={styles.navLink} to={PATH.TESTCOMPONENTS}>
+              <li className={styles.navItem}>
+                Test
+              </li>
+            </NavLink>
+
+          </ul>
+        </nav>
+      </div>
   )
 }
