7dde4a0bbd7a778be040e52a1e66826d9a7f2ba2
whiteartur1997 <whiteartur1997@gmail.com>
2021-03-06 21:38:03 +0200

    hover effect was fixed in Header

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -7,43 +7,30 @@ export const Header = () => {
       <div className={styles.header}>
         <nav className={styles.nav}>
           <ul className={styles.navList}>
-
-            <NavLink className={styles.navLink} to={PATH.PROFILE}>
-              <li className={styles.navItem}>
-                Profile
-              </li>
-            </NavLink>
-
-            <NavLink className={styles.navLink} to={PATH.REGISTRATION}>
-              <li className={styles.navItem}>
-                Registration
-              </li>
-            </NavLink>
-
-            <NavLink className={styles.navLink} to={PATH.LOGIN}>
-              <li className={styles.navItem}>
-                Login
-              </li>
-            </NavLink>
-
-            <NavLink className={styles.navLink} to={PATH.NEWPASSWORD}>
-              <li className={styles.navItem}>
-                New Password
-              </li>
-            </NavLink>
-
-            <NavLink className={styles.navLink} to={PATH.FORGOTPASSWORD}>
-              <li className={styles.navItem}>
-                Forgot Password
-              </li>
-            </NavLink>
-
-            <NavLink className={styles.navLink} to={PATH.TESTCOMPONENTS}>
-              <li className={styles.navItem}>
-                Test
-              </li>
-            </NavLink>
-
+            <div className={styles.header}>
+              <nav className={styles.nav}>
+                <ul className={styles.navList}>
+                  <li className={styles.navItem}>
+                    <NavLink className={styles.navLink} to={PATH.PROFILE}>Profile</NavLink>
+                  </li>
+                  <li className={styles.navItem}>
+                    <NavLink className={styles.navLink} to={PATH.REGISTRATION}>Registration</NavLink>
+                  </li>
+                  <li className={styles.navItem}>
+                    <NavLink className={styles.navLink} to={PATH.LOGIN}>Login</NavLink>
+                  </li>
+                  <li className={styles.navItem}>
+                    <NavLink className={styles.navLink} to={PATH.NEWPASSWORD}>New Password</NavLink>
+                  </li>
+                  <li className={styles.navItem}>
+                    <NavLink className={styles.navLink} to={PATH.FORGOTPASSWORD}>Forgot Password</NavLink>
+                  </li>
+                  <li className={styles.navItem}>
+                    <NavLink className={styles.navLink} to={PATH.TESTCOMPONENTS}>Test</NavLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </ul>
         </nav>
       </div>
--- src/features/password/ForgotPassword/ForgotPassword.tsx
+++ src/features/password/ForgotPassword/ForgotPassword.tsx
@@ -1,3 +1,37 @@
+import {useFormik} from "formik";
+import InputText from "../../../components/InputText/InputText";
+
+type FormikErrorType = {
+  email?: string
+}
+
 export const ForgotPassword = () => {
-  return <div>Forgot Password</div>
+
+  const formik = useFormik({
+    initialValues: {
+      email: ""
+    },
+    validate: (values) => {
+
+      const errors:FormikErrorType = {};
+
+      if(!values.email) {
+        errors.email = "Email is required";
+      } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = "Incorrect email format!";
+      }
+    },
+    onSubmit: () => {
+
+    }
+  })
+
+  return (<div>
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email Address</label>
+     <InputText />
+
+      <button type="submit">Submit</button>
+    </form>
+  </div>)
 }
