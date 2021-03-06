ccd320c593fbca1b28ebbedbf8edff2a1efdc96d
Gotlibbb <gleb.evgenevichh@gmail.com>
2021-03-06 20:41:57 +0300

    bll login

--- src/app/reducers/auth-reducer.ts
+++ src/app/reducers/auth-reducer.ts
@@ -1,8 +1,61 @@
-const initialState = {};
-type AuthStateType = typeof initialState;
-
-export const authReducer = (state: AuthStateType = initialState, action: ActionsType): AuthStateType => {
-  return state
+type InitialStateType = {
+    email: string,
+    password: string,
+    checkRem: boolean,
 }
 
-type ActionsType = any;
+let initialState: InitialStateType = {
+    email: "",
+    password: "",
+    checkRem: false,
+}
+
+export type DispatchActionTypeLogin =
+    | ReturnType<typeof setEmail>
+    | ReturnType<typeof setPassword>
+    | ReturnType<typeof setCheckRem>
+
+
+export const authReducer = (state: InitialStateType = initialState, action: DispatchActionTypeLogin): InitialStateType => {
+
+    switch (action.type) {
+
+        case "SET_EMAIL": {
+            return {...state , email : action.email}
+        }
+        case "SET_PASSWORD": {
+            return {...state , password : action.password}
+        }
+        case "SET_CHEK_REM": {
+            return {...state , checkRem : action.checkRem}
+        }
+        default:
+            return state
+    }
+
+}
+
+export const setEmail = (email: string) => {
+    return {
+        type: "SET_EMAIL",
+        email
+    } as const
+}
+
+
+export const setPassword = (password: string) => {
+    return {
+        type: "SET_PASSWORD",
+        password
+    } as const
+}
+
+
+export const setCheckRem = (checkRem: boolean) => {
+    return {
+        type: "SET_CHEK_REM",
+        checkRem
+    } as const
+}
+
+
--- src/features/Login/LoginContainer.tsx
+++ src/features/Login/LoginContainer.tsx
@@ -1,23 +1,30 @@
 import React, {useState} from "react"
 import Login from "./Login";
+import {useDispatch} from "react-redux";
+import {setCheckRem, setEmail, setPassword} from "../../app/reducers/auth-reducer";
 
 const LoginContainer = () => {
+
   let [inputEmail, setInputEmail] = useState<string>("")
   let [inputPass, setInputPass] = useState<string>("")
-  let [checkRem, setCheckRem] = useState<boolean>(false)
+  let [checkRem, setInputCheckRem] = useState<boolean>(false)
+  let dispatch = useDispatch()
 
   const signInHandler = () => {
+    dispatch(setEmail(inputEmail))
+    dispatch(setPassword(inputPass))
+    dispatch(setCheckRem(checkRem))
 
   }
 
   return <Login
-              inputEmail={inputEmail}
-              inputPass={inputPass}
-              setInputEmail={setInputEmail}
-              setInputPass={setInputPass}
-              checkRem={checkRem}
-              setChekRem={setCheckRem}
-              onSignInHandler={signInHandler}
+      inputEmail={inputEmail}
+      inputPass={inputPass}
+      setInputEmail={setInputEmail}
+      setInputPass={setInputPass}
+      checkRem={checkRem}
+      setChekRem={setInputCheckRem}
+      onSignInHandler={signInHandler}
   />
 }
 
