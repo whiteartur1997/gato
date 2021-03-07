6cbe4d61410cef0e7b4e7bd108720d2225c19db9
Alex <grok88@tut.by>
2021-03-07 11:47:11 +0300

    add Registration.tsx jsx

--- src/features/Registration/Registration.tsx
+++ src/features/Registration/Registration.tsx
@@ -1,3 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Registration.module.css';
+
 export const Registration = () => {
-  return <div>Registration</div>
+    return <div className={styles.registrationPage}>
+        <div className={styles.registrationBlock}>
+            <div>
+                <div>
+                    <label htmlFor="email">Email: </label>
+                    <input type="text" id={'email'}/>
+                </div>
+                <div>
+                    <label htmlFor="pass">Password: </label>
+                    <input type="password" id={'pass'}/>
+                </div>
+                <div>
+                    <label htmlFor="confirmPass">Confirm Password: </label>
+                    <input type="password" id={'confirmPass'}/>
+                </div>
+                <button>Sign up</button>
+            </div>
+            <div>
+                <NavLink to={'login'}> login</NavLink>
+            </div>
+        </div>
+    </div>
 }
