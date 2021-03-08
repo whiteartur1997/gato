e8aa53148cb013384c6a889945ea404e1aeeeace
Alex <grok88@tut.by>
2021-03-08 15:01:30 +0300

    add Formik && validation Registration.tsx

--- src/features/Registration/Registration.tsx
+++ src/features/Registration/Registration.tsx
@@ -1,25 +1,91 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import styles from './Registration.module.css';
+import {useFormik} from 'formik';
+
+/**
+ * Validate formik
+ * @constructor
+ */
+
+type RegistrationFormType = {
+    email: string
+    password: string
+    confirmPassword: string
+}
+type ErrorRegistrationType = {
+    email?: string
+    password?: string
+    confirmPassword?: string
+}
+
+const validate = (values: RegistrationFormType) => {
+    const errors: ErrorRegistrationType = {};
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+
+    if (!values.password) {
+        errors.password = 'Required';
+    }
+    if (!values.confirmPassword) {
+        errors.confirmPassword = 'Required';
+    }
+
+    return errors;
+};
 
 export const Registration = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        },
+        validate,
+        onSubmit: values => {
+            console.log(values);
+        },
+    });
+
     return <div className={styles.registrationPage}>
         <div className={styles.registrationBlock}>
-            <div>
+            <form onSubmit={formik.handleSubmit}>
                 <div>
-                    <label htmlFor="email">Email: </label>
-                    <input type="text" id={'email'}/>
+                    {/*<label htmlFor="email">Email: </label>*/}
+                    <input type="text"
+                           id={'email'}
+                           {...formik.getFieldProps('email')}
+                        placeholder={'Type Email'}
+                    />
+                    {formik.touched.email && formik.errors.email ?
+                        <div style={{color: 'red'}}>{formik.errors.email}</div> : null}
                 </div>
                 <div>
-                    <label htmlFor="pass">Password: </label>
-                    <input type="password" id={'pass'}/>
+                    {/*<label htmlFor="pass">Password: </label>*/}
+                    <input type="password"
+                           id={'pass'}
+                           {...formik.getFieldProps('password')}
+                           placeholder={'Type Password'}
+                    />
+                    {formik.touched.password && formik.errors.password ?
+                        <div style={{color: 'red'}}>{formik.errors.password}</div> : null}
                 </div>
                 <div>
-                    <label htmlFor="confirmPass">Confirm Password: </label>
-                    <input type="password" id={'confirmPass'}/>
+                    {/*<label htmlFor="confirmPass">Confirm Password: </label>*/}
+                    <input type="password"
+                           id={'confirmPass'}
+                           {...formik.getFieldProps('confirmPassword')}
+                           placeholder={'Confirm Password'}
+                    />
+                    {formik.touched.confirmPassword && formik.errors.confirmPassword ?
+                        <div style={{color: 'red'}}>{formik.errors.confirmPassword}</div> : null}
                 </div>
-                <button>Sign up</button>
-            </div>
+                <button type={'submit'}>Sign up</button>
+            </form>
             <div>
                 <NavLink to={'login'}> login</NavLink>
             </div>
